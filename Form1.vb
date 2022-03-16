3261c6f92988a6f79068e152bff00afeeac60436
Muhammad Fareez Borhanudin <fareez@psp.edu.my>
2022-03-16 16:28:24 +0800

    update label name

--- Form1.vb
+++ Form1.vb
@@ -2,7 +2,7 @@
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         MsgBox("Hello World!!")
         lblHello.Text = "Welcome to my first VB Application"
-        lblHello2.Text = "Muhammad Fareez"
+        lblHello2.Text = "Fareez"
     End Sub
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
