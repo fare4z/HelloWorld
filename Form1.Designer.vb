2c6516d509272e1f37ced5450961b1e06faf68be
fare4z <fareez@psp.edu.my>
2022-03-21 08:30:33 +0800

    2

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -305,7 +305,6 @@ Partial Class Form1
         Me.GroupBox1.Size = New System.Drawing.Size(200, 208)
         Me.GroupBox1.TabIndex = 18
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
         '
         'CmbOperator
         '
